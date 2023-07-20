14efb6154d59c4db845e3b4051ca8f637fc416a3
Evgeniy Opryshko <evgeniy.opryshko@gen.tech>
2023-07-20 19:51:33 +0300

    Remove media

--- Package.swift
+++ Package.swift
@@ -25,7 +25,6 @@ let package = Package(
             name: "ARMeasureCamera",
             dependencies: [],
             resources: [
-                .process("Media.xcassets"),
                 .copy("Resources/Focus.scn"),
                 .copy("Resources/target.png")
             ]
